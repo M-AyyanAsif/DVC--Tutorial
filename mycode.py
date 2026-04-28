4e77eb3318c13070b4905500b52893da47fe224a
Ayyan <ayyanasif869@gmail.com>
2026-04-28 16:25:42 +0500

    Second version of data saved

--- mycode.py
+++ mycode.py
@@ -9,8 +9,11 @@ data =  {
 
 df = pd.DataFrame(data)
 
+new_row_loc = {'Name':'AGF1', 'Age':30, 'City': 'Texas'}
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
+df.loc[len(df.index)] = new_row_loc
 
 file_path = os.path.join(data_dir, 'sample_data,csv')
 
